2d2d80576cd16fa500ae5b83d8366c4d6bf019e1
Stefano Castello <steisbetter@outlook.it>
2017-10-25 11:14:32 +0200

    1.3

--- TinyDependencyInjectionContainer/InterfaceResolver.cs
+++ TinyDependencyInjectionContainer/InterfaceResolver.cs
@@ -15,27 +15,29 @@ namespace TinyDependencyInjectionContainer
         {
             try
             {
-                IKernel controllerKernel = new StandardKernel();
+                IKernel controller = new StandardKernel();
+               Dictionary<Type,Type> myDictionary = new Dictionary<Type,Type>();
                 foreach (var line in File.ReadAllLines(str))
                 {
                     if (line.StartsWith("#")) continue;
                     var item = line.Split('*');
-                    var InterfaceAssembly = Assembly.LoadFrom(item[0]);
-                    var ImplementationAssembly = Assembly.LoadFrom(item[2]);
-                    foreach (var type in InterfaceAssembly.GetTypes())
+                    var interfaceAssembly = Assembly.LoadFrom(item[0]);
+                    var implementationAssembly = Assembly.LoadFrom(item[2]);
+                    foreach (var type in interfaceAssembly.GetTypes())
                         if (type.IsInterface && type.FullName.Equals(item[1]))
                         {
                             Console.WriteLine("Todo1 " + type.FullName);
-                            //TODO 1 
+                            foreach (var implType in implementationAssembly.GetTypes())
+                            {
+                                if (implType.IsClass && implType.FullName.Equals(item[3]))
+                                {
+                                    Console.WriteLine("Todo2 " + implType.FullName);
+                                    //TODO Bind !!
+                                    var implementatioInstance = Activator.CreateInstance(implType);
+                                    myDictionary.Add(type,implType);
+                                }
+                            }
                         }
-                    foreach (var implType in ImplementationAssembly.GetTypes())
-                    {
-                        if (implType.IsClass && implType.FullName.Equals(item[3]))
-                        {
-                            Console.WriteLine("Todo2 " + implType.FullName);
-                            //TODO 2
-                        }
-                    }
                 }
             }
             catch (FileNotFoundException e)
@@ -43,22 +45,8 @@ namespace TinyDependencyInjectionContainer
                 Console.WriteLine(e);
                 throw;
             }
-
-
         }
-
-        /*
-         *
-         *Una class library, TinyDependencyInjectionContainer, contenente la classe InterfaceResolver con le seguenti caratteristiche:
-         * il costruttore dovrà ricevere in input un nome di file,
-         * che dovrà essere nel formato sotto esposto, 
-         * che memorizzerà le associazioni interfaccia->implementazione. 
-         * In caso il file non sia nel formato corretto o un assembly/interfaccia/classe indicato nel file non esista, 
-         * il costruttore non dovrà andare a buon fine.
-         * un metodo generico Instantiate:
-         * T Instantiate<T>() where T : class
-         * che dovrà restituire un (nuovo) oggetto della classe che implementa l'interfaccia T, come specificato nel file di configurazione passato al costruttore (oppure null se nel file di configurazione non c'è nessuna associazione per quell'interfaccia)
-*/
+    
         public T Instantiate<T>() where T : class
         {
             return null;

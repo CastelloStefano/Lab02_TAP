5e4a3101f7a236359708ec7b13b5e632b1023937
Stefano Castello <steisbetter@outlook.it>
2017-10-25 09:43:44 +0200

    1.1

---
+++ Application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailSenderInterfaces;
+using TinyDependencyInjectionContainer;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resolver = new InterfaceResolver("TDIC_Configuration.txt");
+            var sender = resolver.Instantiate<IEmailSender>();
+            sender.SendEmail("pippo", "pluto");
+
+            Console.ReadKey();
+
+        }
+    }
+}
--- TinyDependencyInjectionContainer/InterfaceResolver.cs
+++ TinyDependencyInjectionContainer/InterfaceResolver.cs
@@ -20,6 +20,9 @@ namespace TinyDependencyInjectionContainer
          * T Instantiate<T>() where T : class
          * che dovrà restituire un (nuovo) oggetto della classe che implementa l'interfaccia T, come specificato nel file di configurazione passato al costruttore (oppure null se nel file di configurazione non c'è nessuna associazione per quell'interfaccia)
 */
-
+        public T Instantiate<T>() where T : class
+        {
+            return null;
+        }
     }
 }

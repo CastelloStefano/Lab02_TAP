d86e78e0980fd44b8da35dc72b2501983bb1f2df
Stefano Castello <steisbetter@outlook.it>
2017-11-03 15:32:29 +0100

    added Ninject Module V1.0

--- TinyDependencyInjectionContainer/InterfaceResolver.cs
+++ TinyDependencyInjectionContainer/InterfaceResolver.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using Ninject.Modules;
 
 namespace TinyDependencyInjectionContainer
 {
@@ -30,6 +31,8 @@ namespace TinyDependencyInjectionContainer
                         if (type.IsInterface && type.FullName.Equals(item[1]))
                             foreach (var implType in implementationAssembly.GetTypes())
                            if (implType.IsClass && implType.FullName.Equals(item[3]))
+                                    // creo tutte le associazioni possibili
+                                    // le ritorno poi ad ogni richiesta esterna
                                     _myDictionary.Add(type, implType);
                 }
             }
@@ -46,6 +49,22 @@ namespace TinyDependencyInjectionContainer
             _myDictionary.TryGetValue(typeof(T), out var value);
             if (value == null) return null;
             return (T) Activator.CreateInstance(value);
+        }
+    }
+
+    public class GenericModule : NinjectModule
+    {
+        private readonly Type _bindType, _toType;
+
+        public GenericModule(Type bindType, Type toType)
+        {
+            this._bindType = bindType;
+            this._toType = toType;
+        }
+
+        public override void Load()
+        {
+            Bind(_bindType).To(_toType);
         }
     }
 }
